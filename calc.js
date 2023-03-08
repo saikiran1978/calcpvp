31e4597ee50e3f14780fde3804073be5979fb46d
saikiran1978 <68774498+saikiran1978@users.noreply.github.com>
2023-03-08 22:20:28 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+	return x + y;
+};
+	
+function sub(x, y) {
+	return x - y;
+};
+	
+exports.mult = function (x, y) {
+	return x * y;
+};
+	
+exports.div = function (x, y) {
+	return x / y;
+};
